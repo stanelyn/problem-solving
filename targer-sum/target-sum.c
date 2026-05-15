585a5a51b8bc47f9b4940952490e3ba8602a04c4
Stanely <nameirakpamstanelymeitei@gmail.com>
2026-05-15 15:02:40 +0530

    adding target sum program

---
+++ targer-sum/target-sum.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main() {
+    int a[20],i,n,complement;
+    int map[100];
+    for(i=0;i<100;i++)
+    {
+        map[i]=-1;
+    }
+    printf("\nEnter the number of array elements\n");
+    scanf("%d",&n);
+    printf("\nEnter the array elements\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        complement=10-a[i];
+        if(map[complement] != -1)
+            printf("%d and %d",map[complement],i);
+        map[a[i]]=i;
+    }
+    return 0;
+}
